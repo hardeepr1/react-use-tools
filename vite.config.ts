9813c50df7f34eb665709b31c08d3f4d495aa55c
Hardeep Raike <hardeeprai234@gmail.com>
2025-08-11 23:13:31 -0400

    feat: Added vite for bundling

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/hooks/index.ts',
+      name: 'useTools',
+      fileName: (format) => `use-tools.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
